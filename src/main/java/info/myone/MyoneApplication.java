a8a0f30a668c25673eda978d242fb96a52369539
dikalee7 <dikalee7@gmail.com>
2023-06-14 14:59:07 +0900

    first commit

---
+++ src/main/java/info/myone/MyoneApplication.java
@@ -0,0 +1,13 @@
+package info.myone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyoneApplication.class, args);
+	}
+
+}
